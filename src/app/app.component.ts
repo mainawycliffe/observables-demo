debe07aed728b1f6664bc4efe437bc5ea341170e
Maina Wycliffe <wmmaina7@gmail.com>
2019-08-01 01:37:02 +0300

    project initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ObservablesService } from './observables.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observables-demo';
+export class AppComponent implements OnInit, OnDestroy {
+  private subs = new SubSink();
+  behaviourSubjectValues: Date[] = [];
+  behaviourSubjectValues2: Date[] = [];
+  behaviourSubjectValues3: Date[] = [];
+  replaySubjectValues: Date[] = [];
+  replaySubjectValues2: Date[] = [];
+  replaySubjectValues3: Date[] = [];
+
+  interval;
+
+  constructor(private service: ObservablesService) {}
+
+  ngOnInit(): void {}
+
+  start() {
+    this.subs.sink = this.service.subject.subscribe(date =>
+      this.behaviourSubjectValues.push(date)
+    );
+
+    setTimeout(() => {
+      this.subs.sink = this.service.subject.subscribe(date =>
+        this.behaviourSubjectValues2.push(date)
+      );
+    }, 10000);
+
+    setTimeout(() => {
+      this.subs.sink = this.service.subject.subscribe(date =>
+        this.behaviourSubjectValues3.push(date)
+      );
+    }, 20000);
+
+    this.subs.sink = this.service.replaySubject.subscribe(date =>
+      this.replaySubjectValues.push(date)
+    );
+
+    setTimeout(() => {
+      this.subs.sink = this.service.replaySubject.subscribe(date =>
+        this.replaySubjectValues2.push(date)
+      );
+    }, 10000);
+
+    setTimeout(() => {
+      this.subs.sink = this.service.replaySubject.subscribe(date =>
+        this.replaySubjectValues3.push(date)
+      );
+    }, 20000);
+
+    this.interval = setInterval(() => this.service.add(), 1000);
+  }
+
+  reset() {
+    this.behaviourSubjectValues = [];
+    this.behaviourSubjectValues2 = [];
+    this.behaviourSubjectValues3 = [];
+
+    this.replaySubjectValues = [];
+    this.replaySubjectValues2 = [];
+    this.replaySubjectValues3 = [];
+  }
+
+  stop() {
+    this.subs.unsubscribe();
+    clearInterval(this.interval);
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+    clearInterval(this.interval);
+  }
 }
---
+++ src/app/observables.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ObservablesService {
+  subject: BehaviorSubject<Date> = new BehaviorSubject(new Date());
+  replaySubject: ReplaySubject<Date> = new ReplaySubject();
+
+  constructor() {
+    this.replaySubject.next(new Date());
+  }
+
+  add() {
+    this.subject.next(new Date());
+    this.replaySubject.next(new Date());
+  }
+
+  getSubject() {
+    return this.subject;
+  }
+}
