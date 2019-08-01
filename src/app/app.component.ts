3088e5679570149bc733e29218db78834fa118d5
Maina Wycliffe <wmmaina7@gmail.com>
2019-08-01 03:25:10 +0300

    improve ui alitle bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ObservablesService } from './observables.service';
 import { SubSink } from 'subsink';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -24,20 +26,42 @@ export class AppComponent implements OnInit, OnDestroy {
 
   start() {
     this.subs.sink = this.service.subject.subscribe(date =>
-      this.behaviourSubjectValues.push(date)
+      date ? this.behaviourSubjectValues.push(date) : null
     );
 
     setTimeout(() => {
-      this.subs.sink = this.service.subject.subscribe(date =>
-        this.behaviourSubjectValues2.push(date)
-      );
-    }, 10000);
+      this.subs.sink = this.service.subject.subscribe(date => {
+        if (this.behaviourSubjectValues2.length === 0) {
+          // add some extra empty row to the list
+          this.behaviourSubjectValues.forEach(() =>
+            this.behaviourSubjectValues2.push(null)
+          );
+
+          this.behaviourSubjectValues2.pop();
+        }
+
+        if (date !== null) {
+          this.behaviourSubjectValues2.push(date);
+        }
+      });
+    }, 5000);
 
     setTimeout(() => {
-      this.subs.sink = this.service.subject.subscribe(date =>
-        this.behaviourSubjectValues3.push(date)
-      );
-    }, 20000);
+      this.subs.sink = this.service.subject.subscribe(date => {
+        if (this.behaviourSubjectValues3.length === 0) {
+          // add some extra empty row to the list
+          this.behaviourSubjectValues.forEach(() =>
+            this.behaviourSubjectValues3.push(null)
+          );
+
+          this.behaviourSubjectValues3.pop();
+        }
+
+        if (date !== null) {
+          this.behaviourSubjectValues3.push(date);
+        }
+      });
+    }, 10000);
 
     this.subs.sink = this.service.replaySubject.subscribe(date =>
       this.replaySubjectValues.push(date)
@@ -47,18 +71,23 @@ export class AppComponent implements OnInit, OnDestroy {
       this.subs.sink = this.service.replaySubject.subscribe(date =>
         this.replaySubjectValues2.push(date)
       );
-    }, 10000);
+    }, 5000);
 
     setTimeout(() => {
       this.subs.sink = this.service.replaySubject.subscribe(date =>
         this.replaySubjectValues3.push(date)
       );
-    }, 20000);
+    }, 10000);
 
     this.interval = setInterval(() => this.service.add(), 1000);
+
+    // stop after 20 seconds
+    setTimeout(() => this.stop(), 20000);
   }
 
   reset() {
+    this.stop();
+
     this.behaviourSubjectValues = [];
     this.behaviourSubjectValues2 = [];
     this.behaviourSubjectValues3 = [];
@@ -66,6 +95,8 @@ export class AppComponent implements OnInit, OnDestroy {
     this.replaySubjectValues = [];
     this.replaySubjectValues2 = [];
     this.replaySubjectValues3 = [];
+
+    this.service.reset();
   }
 
   stop() {
